cd4fdea06a680f07dc563a131542d5687c2f44d8
coldrook <8768561+coldrook@users.noreply.github.com>
2025-01-19 17:27:49 +0800

    Update log.sh

--- log.sh
+++ log.sh
@@ -6,11 +6,6 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-# 安装必要的软件包
-echo "安装 logrotate, cron 和 rsyslog..."
-sudo apt update
-sudo apt install -y logrotate cron rsyslog
-
 # 获取 /etc/logrotate.d/rsyslog 文件内容
 if [[ ! -f /etc/logrotate.d/rsyslog ]]; then
   echo "错误: /etc/logrotate.d/rsyslog 文件不存在。"
@@ -40,25 +35,37 @@ EOF
 echo "新的配置内容:"
 echo "$NEW_CONFIG"
 
-# 使用 awk 替换 {} 之间的内容 (非贪婪匹配)
-echo "修改 /etc/logrotate.d/rsyslog 文件..."
-MODIFIED_CONFIG=$(echo "$OLD_CONFIG" | awk -v new_config="$NEW_CONFIG" '{
-    print "原始行: " $0
-    gsub(/\{[^\}]*\}/, new_config)
-    print "修改后行: " $0
-    print
-}')
+# 使用 awk 替换配置块
+echo "使用 awk 尝试匹配和替换..."
+MODIFIED_CONFIG=$(echo "$OLD_CONFIG" | awk -v new_config="$NEW_CONFIG" '
+BEGIN {
+    in_block = 0
+}
+/^\{/ {
+    in_block = 1
+    print "找到配置块开始: " $0
+    print new_config
+    next
+}
+/^}/ {
+    in_block = 0
+    print "找到配置块结束: " $0
+    next
+}
+{
+    if (!in_block) {
+        print
+    } else {
+        print "忽略行: " $0
+    }
+}
+')
 echo "修改后的配置内容:"
 echo "$MODIFIED_CONFIG"
 
 # 将修改后的内容写回文件
 echo "$MODIFIED_CONFIG" | sudo tee /etc/logrotate.d/rsyslog > /dev/null
 echo "写入文件完成"
-
-# 检查 logrotate 服务状态
-echo "检查 logrotate 服务状态..."
-sudo systemctl status logrotate.service
-sudo systemctl status logrotate.timer
 
 # 检查文件内容
 echo "修改后的 /etc/logrotate.d/rsyslog 文件内容:"
