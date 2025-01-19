a66c560c679a84c3806f8ea8b777da8401f2f829
coldrook <8768561+coldrook@users.noreply.github.com>
2025-01-19 16:57:24 +0800

    Update log.sh

--- log.sh
+++ log.sh
@@ -38,7 +38,7 @@ EOF
 
 # 替换 {} 之间的内容
 echo "修改 /etc/logrotate.d/rsyslog 文件..."
-MODIFIED_CONFIG=$(echo "$OLD_CONFIG" | sed -E -z "s@\{[^}]*\}@${NEW_CONFIG//@/\\@}@g")
+MODIFIED_CONFIG=$(echo "$OLD_CONFIG" | sed -E -z 's@\{[^}]*\}@'"$NEW_CONFIG"'@')
 
 # 将修改后的内容写回文件
 echo "$MODIFIED_CONFIG" | sudo tee /etc/logrotate.d/rsyslog > /dev/null
